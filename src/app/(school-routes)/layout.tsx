d9fa407e6a1147ec7c7dc124cf2598c3323580e0
John DoeTDZHINNN <thiagoil2006@gmail.com>
2026-07-04 18:25:08 -0300

    feat: implement SchoolLayout and SchoolPage components with session management and dashboard view

---
+++ src/app/(school-routes)/layout.tsx
@@ -0,0 +1,24 @@
+import { getServerSession } from "next-auth";
+import { ReactNode } from "react";
+import { nextAuthOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+interface PrivateLayoutProps {
+    children: ReactNode;
+}
+
+// Quem não tem uma sessão ativa vai ser redirecionado para page de login
+export default async function SchoolLayout({ children }: PrivateLayoutProps) {
+    const session = await getServerSession(nextAuthOptions)
+
+    // uma 2º camada de proteção alem do middleware
+    if(!session) {
+        redirect('/login')
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
